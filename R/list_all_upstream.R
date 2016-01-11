9a16a41831010dcb3afb48fb9516d1900176a95a
Nick Bond <n.bond@griffith.edu.au>
2016-01-12 10:05:23 +1100

    renamed function

--- R/list_all_upstream.R
+++ R/list_all_upstream.R
@@ -1,21 +1,21 @@
 #' Identify all the subcatchments upstream of a list of specified catchments
-#' 
+#'
 #' @param hierarchy a dataframe containing catchment id and next downstream (nextds) id fields
 #' @param catchnames a vector of catchment ids for which a a list of upstream catchment
 #' ids will be returned.
 #' @return a list of upstream catchment ids for each catchment in catchnames
 #' @note Function depends on the next downstream field in a stream network 'hierarchy' table (dataframe).
-#' Can be used to support further aggregation of environmental variables for sub-catchments downstream 
+#' Can be used to support further aggregation of environmental variables for sub-catchments downstream
 #' of a list of catchments of interest (e.g. for calculating barrier numbers).
 #' @examples
-#' 
+#'
 #' @export
 
-list.all.upstream<-function(cat.hier, catchnames) {
-  
+list_all_upstream<-function(cat.hier, catchnames) {
+
   all.us.sites<-vector("list",length(catchnames))
-  
-  for (i in 1:length(catchnames)) 
+
+  for (i in 1:length(catchnames))
   {
     us.sites<-allupstream(cat.hier,catchnames[i])
     all.us.sites[[i]]<-us.sites
