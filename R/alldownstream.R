247ef0e0fc9a50a89266310ac5d5014de3422793
Nick Bond <n.bond@griffith.edu.au>
2016-03-01 15:07:28 +1100

    changed hierarchy to cat.hier

--- R/alldownstream.R
+++ R/alldownstream.R
@@ -1,22 +1,22 @@
 #' Return a vector of all subcatchments downstream of a specified catchment
 
-#' @param hierarchy a dataframe containing catchment id and next downstream (nextds) id fields
-#' @param catchname a dataframe containing catchment id for which a vector of downstream catchment 
+#' @param cat.hier a dataframe containing catchment id and next downstream (nextds) id fields
+#' @param catchname a dataframe containing catchment id for which a vector of downstream catchment
 #' ids will be returned.
 #' @return a vector of downstream catchment ids
 #' @note Function depends on the next downstream field in a stream network 'hierarchy' table (dataframe).
-#' When used in conjunction with list_all_downstream it is possible to get a list of catchments downstream 
+#' When used in conjunction with list_all_downstream it is possible to get a list of catchments downstream
 #' of a vector of sites. This can then be used to support further aggregation of environmental variables
 #' for sub-catchments downstream of a list of catchments of interest (e.g. for calculating barrier numbers).
 #' @examples
-#' 
+#'
 #' @export
 
-alldownstream <- function(hierarchy, catchname){
+alldownstream <- function(cat.hier, catchname){
   if(length(which(hierarchy$site==catchname))>0)
   {
     catchname <- as.vector(catchname)
-    allsc <- as.vector(hierarchy$nextds[hierarchy$site==catchname])
+    allsc <- as.vector(cat.hier$nextds[cat.hier$site==catchname])
     allsc <- allsc[!is.na(allsc)]
     #subcatchments immediately upstream
     nbrnch <- end <- length(allsc)
@@ -26,12 +26,12 @@ alldownstream <- function(hierarchy, catchname){
     {
       for(j in start:end)
       {
-        allsc <- c(allsc,as.vector(hierarchy$nextds[hierarchy$site == allsc[j]]))
+        allsc <- c(allsc,as.vector(cat.hier$nextds[cat.hier$site == allsc[j]]))
         allsc <- allsc[!is.na(allsc)]
       }
       start <- end + 1
       end <- length(allsc)
-      nbrnch <- end - (start - 1) 
+      nbrnch <- end - (start - 1)
     }
     allsc <- c(catchname,allsc)
     allsc <- allsc[allsc != -1]
