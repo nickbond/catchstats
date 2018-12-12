2ba5dbc6ad88a883cb8aa42364e803149303a571
nickbond <n.bond@latrobe.edu.au>
2018-12-12 13:00:08 +1000

    updated to allow aggregation based on statistics other than the mean for each polygon (e.g. can now use max, min etc).

--- R/extractRasterPoly.R
+++ R/extractRasterPoly.R
@@ -3,8 +3,14 @@
 #' @param shpfile The shapefile to be used to intersect with the raster layers
 #' @param rast The raster layer(s) to be intersected
 #' @param catid_col the catchment id field in the shpfile
+#' @param fun The function to apply. Requires a single value is returned
+#' (e.g. fun = mean, fun = max). see `raster::extract`` for more details
+#' @param weights see `Raster::extract`
+#' @param normalizeWeights see `Raster::extract`
+#' @param na.rm see `Raster::extract`
+#' @param small see `Raster::extract`
 #' @param start the first polygon in the shapefile to be used for intersection
-#' Defaults to a value of 1 (the first row).
+#' Defaults to a value of 1 (the first row)
 #' @param end The last polygon in the shapfile to be intersected.
 #' @return a dataframe of area-weighted raster values for each polygon.
 #'
@@ -13,7 +19,7 @@
 #' #Define a bounding box for the city of Melbourne, Australia and surrounds
 #' melb <- list(x = c(144.0000, 146.5000), y = c(-39.0000, -37.0000))
 #' awap_stack <- stack_awap(bbox=melb, start='20130131')
-#' vic_cats<-readShapePoly('/Users/nickbond/Data/GIS_Data/Australia/BaseData/Catchments/VIC/VIC_CATCHMENTS.shp')
+#' vic_cats<-readShapePoly('/Users/nbond/Data/GIS_Data/Australia/BaseData/Catchments/VIC/VIC_CATCHMENTS.shp')
 #' proj4string(vic_cats) <- CRS('+init=epsg:4283')
 #' vic_cats <- spTransform(vic_cats, CRS('+init=epsg:28355'))
 #' vic_cats <- vic_cats[which(vic_cats@data$BNAME %in% c('YARRA RIVER', 'MARIBYRNONG RIVER')),]
@@ -25,21 +31,21 @@
 #' @export
 
 
-extractRasterPoly <- function(shpfile, rast, catid_col, start = 1, end = nrow(shpfile)) {
+extractRasterPoly <- function(shpfile, rast, catid_col, fun = mean, weights = TRUE, normalizeWeights = TRUE, na.rm=TRUE, small = TRUE, start = 1, end = nrow(shpfile)) {
 
-    # vector('list', length(start:end))
+  # vector('list', length(start:end))
 
-    loc.values <- vector("list", length(start:end))
-    shpfile <- shpfile[start:end, ]
+  loc.values <- vector("list", length(start:end))
+  shpfile <- shpfile[start:end, ]
 
-    for (i in 1:length(shpfile)) {
-        poly <- shpfile[i, ]
-        loc.values[[i]] <- raster::extract(rast, poly, na.rm = T, weights = TRUE, fun = "mean", normalizeWeights = TRUE, small = TRUE)
-        print(i)
-    }
+  for (i in 1:length(shpfile)) {
+    poly <- shpfile[i, ]
+    loc.values[[i]] <- raster::extract(rast, poly, na.rm = na.rm, weights = weights, fun = fun, normalizeWeights = normalizeWeights, small = small)
+    print(i)
+  }
 
-    loc.values.df <- as.data.frame(t(do.call("rbind", loc.values)))
-    names(loc.values.df) <- shpfile@data[, catid_col]
-    return(loc.values.df)
+  loc.values.df <- as.data.frame(t(do.call("rbind", loc.values)))
+  names(loc.values.df) <- shpfile@data[, catid_col]
+  return(loc.values.df)
 }
 
--- R/extractRasterPoly_cl.R
+++ R/extractRasterPoly_cl.R
@@ -3,24 +3,30 @@
 #' @param shpfile The shapefile to be used to intersect with the raster layers
 #' @param rast The raster layer(s) to be intersected
 #' @param catid_col the catchment id field in the shpfile
+#' @param fun The function to apply. Requires a single value is returned
+#' (e.g. fun = mean, fun = max). see `raster::extract`` for more details
+#' @param weights see `Raster::extract`
+#' @param normalizeWeights see `Raster::extract`
+#' @param na.rm see `Raster::extract`
+#' @param small see `Raster::extract`
 #' @param start the first polygon in the shapefile to be used for intersection
-#' Defaults to a value of 1 (the first row).
+#' Defaults to a value of 1 (the first row)
 #' @param end The last polygon in the shapfile to be intersected.
 #' @return a dataframe of area-weighted raster values for each polygon.
-#'
+'
 #' @examples
 #'
 #' #Note the code below will only work on Mac/Unix based systems.
 #' #Other implementations of the parallel processors (e.g. PSOCK) may work on Windows but are untested.
 #' #The following to lines are required to initiate the parallel processors
-#' 
+#'
 #' library(parallel)
 #' no_cores=detectCores()-1
 #'
 #' #Define a bounding box for the city of Melbourne, Australia and surrounds
 #' melb <- list(x = c(144.0000, 146.5000), y = c(-39.0000, -37.0000))
 #' awap_stack <- stack_awap(bbox=melb, start='20130131')
-#' vic_cats<-readShapePoly('/Users/nickbond/Data/GIS_Data/Australia/BaseData/Catchments/VIC/VIC_CATCHMENTS.shp')
+#' vic_cats<-readShapePoly('/Users/nbond/Data/GIS_Data/Australia/BaseData/Catchments/VIC/VIC_CATCHMENTS.shp')
 #' proj4string(vic_cats) <- CRS('+init=epsg:4283')
 #' vic_cats <- spTransform(vic_cats, CRS('+init=epsg:28355'))
 #' vic_cats <- vic_cats[which(vic_cats@data$BNAME %in% c('YARRA RIVER', 'MARIBYRNONG RIVER')),]
@@ -31,11 +37,11 @@
 #'
 #' @export
 
-extractRasterPoly_cl <- function(shpfile, rast, catid_col, start = 1, end = nrow(shpfile)) {
+extractRasterPoly_cl <- function(shpfile, rast, catid_col, fun = mean, weights = TRUE, normalizeWeights = TRUE, na.rm=TRUE, small = TRUE, start = 1, end = nrow(shpfile)) {
   loc.values <- vector("list", length(start:end))
   shpfile <- shpfile[start:end, ]
   cl<-parallel::makeCluster(no_cores, type="FORK")
-  fits<-parallel::parLapplyLB(cl,1:length(shpfile),function(i) loc.values[[i]] <- raster::extract(rast, shpfile[i,], na.rm = T, weights = TRUE, fun = "mean", normalizeWeights = TRUE, small = TRUE))
+  fits<-parallel::parLapplyLB(cl,1:length(shpfile),function(i) loc.values[[i]] <- raster::extract(rast, poly, na.rm = na.rm, weights = weights, fun = fun, normalizeWeights = normalizeWeights, small = small)
   parallel::stopCluster(cl)
   loc.values.df <- as.data.frame(t(do.call("rbind", fits)))
   names(loc.values.df) <- shpfile@data[, catid_col]
